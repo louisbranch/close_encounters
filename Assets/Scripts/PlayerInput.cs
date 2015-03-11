80553c149306880e816219365e1aa7949ae15a4c
Luiz Branco <me@luizbranco.com>
2015-03-11 01:15:16 -0400

    Include debug key press

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+	private void Update () {
+		foreach (char c in Input.inputString) {
+			Debug.Log (c);
+		}
+	}
+
+}
