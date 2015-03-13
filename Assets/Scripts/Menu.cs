33ac7fa5f1f729a1b274ff9bfd5ddfb41786b019
Christopher Aytona <christopher.aytona@hotmail.com>
2015-03-13 13:28:02 -0400

    Basic MainMenu UI added

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public void StartGame () {
+		Application.LoadLevel("Level1");
+	}
+	
+	public void MainMenu () {
+		Application.LoadLevel("MainMenu");
+	}
+	
+	public void Credits () {
+		Application.LoadLevel("Credits");
+	}
+	
+	public void HowTo () {
+		Application.LoadLevel("HowTo");
+	}
+	
+	public void Quit () {
+		Application.Quit();
+	}
+}
