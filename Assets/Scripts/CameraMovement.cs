01711dfdd0e144431ffe3ea73959132078f40ac6
Christopher Aytona <christopher.aytona@hotmail.com>
2015-03-13 17:28:40 -0400

    Space Skybox added

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Transform cameraPosition;
+	public float speed;
+
+	public void Update() {
+		if (cameraPosition != null) {
+			transform.RotateAround(cameraPosition.position, Vector3.up, speed * Time.deltaTime);
+		}
+	}
+}
