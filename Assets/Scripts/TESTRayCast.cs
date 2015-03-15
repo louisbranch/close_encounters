36c30d93c63260a9975ce830864453b9e7cf0532
Wahid Shafique <wahidshafique@hotmail.com>
2015-03-14 22:23:40 -0400

    Fixed RayCast
    Each instantiated word emits a ray towards player. Viewable in scene
    mode for debug purposes

---
+++ Assets/Scripts/TESTRayCast.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TESTRayCast : MonoBehaviour {
+	public bool wordInView = false;
+	public Transform startPoint, endPoint;
+	RaycastHit2D wordHit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Laser();
+	}
+	void Laser () {
+		Debug.DrawLine(startPoint.position, endPoint.position, Color.red);
+		if (Physics2D.Linecast(startPoint.position, endPoint.position, 1 << LayerMask.NameToLayer("Word"))) {
+			wordHit = Physics2D.Linecast(startPoint.position, endPoint.position, 1 << LayerMask.NameToLayer("Word"));
+			wordInView = true; 
+		} else {
+			wordInView = false;
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Destroy (wordHit.collider.gameObject);
+		}
+	}
+}
