f0349e15bf53eba56cc57ab339819d27f8ee65f6
Christopher Aytona <christopher.aytona@hotmail.com>
2015-03-16 15:32:29 -0400

    Music + SideStep

--- Assets/Scripts/Checker.cs
+++ Assets/Scripts/Checker.cs
@@ -3,30 +3,13 @@ using System.Collections;
 
 public class Checker : MonoBehaviour {
 
+	public AudioClip Ouch;
 
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Word"){
-		Destroy(other.gameObject);
-		PlayerData.Instance.Health--;
+			AudioSource.PlayClipAtPoint (Ouch, Vector3.zero);
+			Destroy(other.gameObject);
+			PlayerData.Instance.Health--;
 		}
 	}
-		/*public float maxGravDist = 4.0f;
-		public float maxGravity = 35.0f;
-		
-		GameObject[] players;
-		
-		void Start () {
-			player = GameObject.FindGameObjectsWithTag("Player");
-		}
-		
-		void FixedUpdate () {
-			foreach(GameObject player in players) {
-				float dist = Vector3.Distance(player.transform.position, transform.position);
-				if (dist <= maxGravDist) {
-					Vector3 v = player.transform.position - transform.position;
-					Rigidbody2D.AddForce(v.normalized * (1.0f - dist / maxGravDist) * maxGravity);
-				}
-			}
-		}
-}*/
 }
---
+++ Assets/Scripts/SideStepEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideStepEffect : MonoBehaviour {
+
+	private float startX = 0.5f;
+	public float duration = 1f;
+	
+	void Start () {
+		startX = transform.position.x;
+	}
+	
+	void Update () {
+		float newX = startX + (startX + Mathf.Cos (Time.time / duration * 2));
+		transform.position = new Vector2 (newX , transform.position.y);
+	}
+}
