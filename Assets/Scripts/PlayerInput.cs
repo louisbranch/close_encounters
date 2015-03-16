e17f19b81403fe1a3303cbde846f8735b03d2f31
Christopher Aytona <christopher.aytona@hotmail.com>
2015-03-16 14:25:23 -0400

    Player Sound

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,25 +1,25 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerInput : MonoBehaviour {
 
-	const string LETTERS = "abcdefghijklmnopqrstuvwxyz";
+	public AudioClip laser;
+	public AudioClip error;
 
-	AudioSource source;
+	const string LETTERS = "abcdefghijklmnopqrstuvwxyz";
 	
 	[SerializeField] private WordManager manager;
-
-	private void Awake () {
-		source = GetComponent<AudioSource>();
-	}
 
 	private void Update () {
 		foreach (char c in Input.inputString) {
 			if (LETTERS.Contains(c.ToString())) {
 				GameObject target = manager.DestroyLetter(c);
 				if (target != null) {
-					source.Play();
+					AudioSource.PlayClipAtPoint (laser, Vector3.zero);
 					PlayerData.Instance.Score++;
+				}
+				if (target == null) {
+					AudioSource.PlayClipAtPoint (error, Vector3.zero);
 				}
 			}
 		}
