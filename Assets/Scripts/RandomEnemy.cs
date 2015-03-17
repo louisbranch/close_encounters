155cf1f0901b7a813d9791a333c9fb520d0d9452
aytona <christopher.aytona@hotmail.com>
2015-03-16 22:34:37 -0400

    Enemy Prefab Update

---
+++ Assets/Scripts/RandomEnemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomEnemy : MonoBehaviour {
+
+	public Sprite[] enemies;
+	public string enemyResource;
+	public int currentEnemy = -1;
+
+	void Start() {
+		if (enemyResource != "") {
+			enemies = Resources.LoadAll<Sprite>(enemyResource);
+			if (currentEnemy == -1) {
+				currentEnemy = Random.Range(0, enemies.Length);
+			}
+			else if (currentEnemy > enemies.Length) {
+				currentEnemy = enemies.Length - 1;
+			}
+			GetComponent<SpriteRenderer>().sprite = enemies[currentEnemy];
+		}
+	}
+}
