fc1ddedf1bbb9f2ae370ee4dd7408c82d4fb19fc
aytona <christopher.aytona@hotmail.com>
2015-03-16 21:18:45 -0400

    Continuous Music in Menu

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroy : MonoBehaviour {
+
+	private static DontDestroy instance = null;
+	public static DontDestroy Instance {
+		get { return instance; }
+	}
+	
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		}
+		else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	void OnLevelWasLoaded() {
+		if(Application.loadedLevelName == "Level1") {
+			Destroy(this.gameObject);
+		}
+	}
+}
